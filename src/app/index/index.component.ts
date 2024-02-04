080c97e995857da81442f4f5a61507d80bbb81f1
sadatrafsanjani <sadatrafsanjani@gmail.com>
2024-02-05 00:16:44 +0600

    ui stable

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,17 +1,18 @@
-import {Component} from '@angular/core';
+import {Component } from '@angular/core';
 import {NavComponent} from "../template/nav/nav.component";
 import {FooterComponent} from "../template/footer/footer.component";
 import {RateComponent} from "../template/rate/rate.component";
 
-
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [NavComponent, FooterComponent, RateComponent],
+  imports: [
+    NavComponent,
+    FooterComponent,
+    RateComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
-
 
 }
--- src/app/template/rate/rate.component.ts
+++ src/app/template/rate/rate.component.ts
@@ -1,83 +1,41 @@
-import {Component, ElementRef, Inject, OnInit, ViewChild} from '@angular/core';
-import {DOCUMENT, NgForOf} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
 import {HttpClientModule} from "@angular/common/http";
 import {CurrencyService} from "../../service/currency.service";
-import {Chart} from "chart.js";
 
 
 @Component({
   selector: 'app-rate',
   standalone: true,
-  imports: [NgForOf, HttpClientModule],
+  imports: [
+    NgForOf,
+    HttpClientModule,
+    NgIf],
   templateUrl: './rate.component.html',
   styleUrl: './rate.component.css'
 })
 export class RateComponent implements OnInit {
 
+  rates: any = [];
 
-
-  public rates: any = [];
-
-  canvas: any;
-  ctx: any;
-
-  constructor(readonly currencyService: CurrencyService, @Inject(DOCUMENT) private document: Document) {
-    this.canvas = this.document.getElementById('rateChart');
-
-    if(this.canvas){
-
-      this.ctx = this.canvas.getContext('2d');
-    }
+  constructor(private currencyService: CurrencyService) {
   }
 
   ngOnInit(): void {
 
+
     this.currencyService.getExchangeRate().subscribe((response: any) => {
-      const data = response.toString().replace(/\s+/g, '').trim();
-      this.rates = this.extractData(data);
-      this.loadChart();
-    });
-
-  }
-
-  loadChart(){
-
-    let label = [];
-    let buys = [];
-    let sells = [];
-
-    for(let i=0; i<this.rates.length; i++){
-
-      label.push(this.rates[i][0]);
-      buys.push(this.rates[i][1]);
-      sells.push(this.rates[i][2]);
-    }
-
-    new Chart(this.canvas, {
-      type: 'bar',
-      data: {
-        labels: label,
-        datasets: [{
-          label: '# Currency',
-          data: buys,
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
+      if(response != null){
+        const data = response.toString().replace(/\s+/g, '').trim();
+        this.extractData(data);
       }
     });
   }
 
-  extractData(table: string){
+  private extractData(table: string){
 
     let array: any = [];
     let data: any = [];
-    let allData: any = [];
 
     let m: number | RegExpExecArray | null;
     const regex = /<td>(.*?)<\/td>/gm;
@@ -102,17 +60,16 @@ export class RateComponent implements OnInit {
 
       if(i%3 == 0){
 
-        const t = {
+        const row = {
           currency: data[0],
           buy: data[1],
           sell: data[2]
         };
 
-        allData.push(t);
+        this.rates.push(row);
         data = [];
       }
     }
-
-    return allData;
   }
+
 }
